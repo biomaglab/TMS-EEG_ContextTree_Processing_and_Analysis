9af118c8d3bc2fc4eb90925783947ced0266aeed
biomaglab <biomaglab@outlook.com>
2026-05-25 15:35:06 -0300

    Add  EEG exports and harmonic notch filtering

--- src/tms_eeg/config/settings.py
+++ src/tms_eeg/config/settings.py
@@ -2,8 +2,8 @@ from dataclasses import dataclass, field
 
 @dataclass
 class IOConfig:
-    export_data: bool = False
-    save_figs: bool = False
+    export_data: bool = True
+    save_figs: bool = True
 
 @dataclass
 class EventConfig:
@@ -20,7 +20,7 @@ class EventConfig:
 
 @dataclass
 class ArtifactConfig:
-    window_removal_artifact: tuple = (-0.005, 0.008)
+    window_removal_artifact: tuple = (-0.002, 0.010)
     mode_removal_artifact: str = 'cubic'
     anchor_window_ms: float = 5.0
 
